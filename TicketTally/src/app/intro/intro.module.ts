5ce889a1b99ba171fb9d9f48bda483f7459b4943
Jonathan Cantillo <jonathan.cantillob@pca.edu.co>
2024-01-22 17:27:08 -0500

    New

--- TicketTally/src/app/intro/intro.module.ts
+++ TicketTally/src/app/intro/intro.module.ts
@@ -7,6 +7,8 @@ import { IonicModule } from '@ionic/angular';
 import { IntroPageRoutingModule } from './intro-routing.module';
 
 import { IntroPage } from './intro.page';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 
 @NgModule({
   imports: [
@@ -15,6 +17,7 @@ import { IntroPage } from './intro.page';
     IonicModule,
     IntroPageRoutingModule
   ],
-  declarations: [IntroPage]
+  declarations: [IntroPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class IntroPageModule {}
--- TicketTally/src/app/intro/intro.page.ts
+++ TicketTally/src/app/intro/intro.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
@@ -7,8 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroPage implements OnInit {
 
-  constructor() { }
+  slides = [
+    {
+      title: "BIENVENIDO A TU DESTINO CINEMATOGRAFICO",
+      image: "../../assets/imagenes/wonka.jpg",
+      description: "Sumérgete en un universo de historias infinitas y experiencias inigualables.",
+      pin: "¿Que te gustaria ver hoy? ",
+      texto_ayuda: "Desliza para continuar o pulsa el boton",
+      class: "slide-1"
+    },
 
+    {
+      title: " MISIÓN ",
+      image: "../../assets/imagenes/cine.jpg",
+      description: "Ofrecer una plataforma digital que celebre y promueva la magia del cine, proporcionando a los amantes del séptimo arte un espacio donde puedan descubrir, explorar y conectarse con películas de diversas épocas, géneros y culturas. Nuestra misión es compartir la pasión por el cine mediante reseñas, análisis profundos,noticias frescas y contenido exclusivo, inspirando a una comunidad global a apreciar, reflexionar y disfrutar del arte cinematográfico en todas sus formas.",
+      texto_ayuda: "Desliza para continuar",
+      class: "slide-2"
+    },
+
+    {
+      title: "COMENCEMOS",
+      description: "Haz clic aquí para conocer más ",
+      class: "slide-3"
+    }
+  ]
+
+  constructor(private router: Router) { }
+
+  goToHome(){
+    console.log("go To Home");
+    this.router.navigateByUrl('/home');
+  }
   ngOnInit() {
   }
 
