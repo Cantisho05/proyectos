7094e70124afde554d5e6103fbdf53aaf6906a79
Jonathan Cantillo <jonathan.cantillob@pca.edu.co>
2024-01-24 00:00:14 -0500

    23

--- TicketTally/src/app/intro/intro.page.ts
+++ TicketTally/src/app/intro/intro.page.ts
@@ -11,7 +11,7 @@ export class IntroPage{
 
   slides = [
     {
-      title: "¡Bienvenido a TicketTally!",
+      title: "¡BIEVENIDO A TicketTally!",
       image: "../../assets/imagenes/Imagen1.jpg",
       description: "Encuentra tu proximo destino y descubre las mejores ofertas. ",
       texto_ayuda: "Desliza para continuar.",
@@ -21,15 +21,15 @@ export class IntroPage{
     {
       title: " VUELOS A UN TAP! ",
       image: "../../assets/imagenes/Imagen2.jpg",
-      description:"" ,
+      description:"Explora nuevos horizontes con nuestros vuelos a un toque de distancia. Descubre experiencias únicas y destinos fascinantes con tan solo un clic." ,
       texto_ayuda: "Desliza para continuar",
       class: "slide-2"
     },
 
     {
-      title: " COMENCEMOS ",
-      image: "../../assets/imagenes/imagen3.jpg,",
-      description: "Haz clic aquí para conocer más ",
+      title: " ¡COMENCEMOS! ",
+      image: "../../assets/imagenes/imagen3.jpg",
+      description: " ¡Haz clic aquí para conocer más y comienza tu próxima aventura ahora mismo! ",
       class: "slide-3"
     }
   ]
